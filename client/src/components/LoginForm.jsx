f61fe375f579af5ad5c8d573734c7dd771196733
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-05-10 22:07:34 -0500

    [fix] slight cleanup on LoginForm, preps for react-router

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -17,59 +17,6 @@ const styles = theme => ({
   margin: {
     margin: theme.spacing.unit,
   },
-  cssLabel: {
-    '&$cssFocused': {
-      color: purple[500],
-    },
-  },
-  cssFocused: {},
-  cssUnderline: {
-    '&:after': {
-      borderBottomColor: purple[500],
-    },
-  },
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-      borderColor: purple[500],
-    },
-  },
-  notchedOutline: {},
-  bootstrapRoot: {
-    'label + &': {
-      marginTop: theme.spacing.unit * 3,
-    },
-  },
-  bootstrapInput: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.common.white,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    width: 'auto',
-    padding: '10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-  bootstrapFormLabel: {
-    fontSize: 18,
-  },
 });
 
 const theme = createMuiTheme({
@@ -89,7 +36,7 @@ function LoginForm(props) {
         <TextField
           className={classes.margin}
           label="first name"
-          id="mui-theme-provider-standard-input"
+          id="first-name"
           required={true}
         />
       </MuiThemeProvider>
@@ -97,7 +44,7 @@ function LoginForm(props) {
         <TextField
           className={classes.margin}
           label="last name"
-          id="last name"
+          id="last-name"
           required={true}
         />
       </MuiThemeProvider>
@@ -113,7 +60,7 @@ function LoginForm(props) {
         <TextField
           className={classes.margin}
           label="password"
-          id="mui-theme-provider-standard-input"
+          id="password"
           required={true}
           type="password"
         />
